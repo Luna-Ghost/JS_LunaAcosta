78b9cbb443765586dbf23a5b219b0105d756a6a3
Luna <lunifer2004@gmail.com>
2021-05-14 14:21:13 -0500

    FEAT: fibonacci subido (actividad 2)

---
+++ Actividad 2/index.js
@@ -0,0 +1,31 @@
+const columnas = Math.floor(Math.random() * 10) + 1;
+const filas = Math.floor(Math.random() * 10) + 1;
+console.log("Alto: " + columnas);
+console.log("Ancho: " + filas);
+
+let inicio = 1;
+num=1;
+fibonacci=1;
+let sucesion = [];
+
+for(let i = 0; i < columnas; i++)
+{
+    sucesion[i] = new Array();
+    for(let j = 0; j < filas; j++)
+    {
+        if(i==0 && j==0)
+        {
+            sucesion[i][j]=inicio;
+        }else{
+            fibonacci = num + fibonacci;
+            num = fibonacci - num;
+            sucesion[i][j]=num;
+        } 
+    }
+    if(i % 2 == 1)
+    {
+        sucesion[i].reverse();
+    }
+}
+
+console.log(sucesion);
